b06b3212c6b6a34d4f3060305ce3160f5423622a
Jakob Holderbaum <privat@techfolio.de>
2010-09-17 07:20:08 +0200

    stupid solution, just kidding

--- lib/wormhole/config.rb
+++ lib/wormhole/config.rb
@@ -4,6 +4,9 @@ module Wormhole
     # this looks like a dirty woraround, but I'm thinking about some kind of a dynamic alias-system vie method_missing
     #  but until now, we will use wormhole_*
 
+    def wormhole_size
+      0
+    end
 
   end
 end
