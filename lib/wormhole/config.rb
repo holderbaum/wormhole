27b65fd3b9f87909252536997562828b35fbde02
Jakob Holderbaum <privat@techfolio.de>
2010-09-17 07:17:36 +0200

    added a brief description of the prefix-technique

--- lib/wormhole/config.rb
+++ lib/wormhole/config.rb
@@ -1,4 +1,9 @@
 module Wormhole
   class Config
+    # since we want a struct like concatination of indexes, every public method will be prefixed by wormhole_
+    # this looks like a dirty woraround, but I'm thinking about some kind of a dynamic alias-system vie method_missing
+    #  but until now, we will use wormhole_*
+
+
   end
 end
