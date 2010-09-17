828959f053bd8cc8c54b7100d2a1ddd30f6cb274
Jakob Holderbaum <privat@techfolio.de>
2010-09-17 07:21:57 +0200

    hash functionality

--- spec/wormhole_config_spec.rb
+++ spec/wormhole_config_spec.rb
@@ -9,5 +9,10 @@ describe Wormhole::Config do
   it "should be empty on creation" do
     @config.wormhole_size.should == 0
   end
+
+  it "should respond like a hash to []" do
+    @config[:index] = "value"
+    @config[:index].should == "value"
+  end
 end
 
