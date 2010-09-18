8be991516d5a17b5090e0218d06f154bdc2651e5
Jakob Holderbaum <privat@techfolio.de>
2010-09-18 22:32:49 +0200

    implemented *?

--- lib/wormhole/config.rb
+++ lib/wormhole/config.rb
@@ -8,6 +8,8 @@ module Wormhole
     def method_missing(meth, *args, &blk)
       if meth.to_s =~ /[a-zA-Z0-9]*=/
         return @hash[meth.to_s[0..-2].to_sym] = args[0]
+      elsif meth.to_s =~ /[a-zA-Z0-9]*\?/
+        return !@hash[meth.to_s[0..-2].to_sym].nil?
       elsif @hash[meth].nil?
         return @hash[meth] = self.class.new
       else
