c53d594159eead40ad4c720487935f6fe508e800
Jakob Holderbaum <privat@techfolio.de>
2010-09-18 11:51:57 +0200

    met the new reqs

--- lib/wormhole/config.rb
+++ lib/wormhole/config.rb
@@ -4,8 +4,20 @@ module Wormhole
     # this looks like a dirty woraround, but I'm thinking about some kind of a dynamic alias-system vie method_missing
     #  but until now, we will use wormhole_*
 
+    def initialize
+      @hash = {}
+    end
+
     def wormhole_size
       0
+    end
+
+    def []=(key,value)
+      @hash[key] = value
+    end
+
+    def [](key)
+      @hash[key] ||= self.class.new
     end
 
   end
