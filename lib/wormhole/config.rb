b98c4b95a3a48d0915216c07fd8796d3def7a28e
Jakob Holderbaum <privat@techfolio.de>
2010-09-18 21:35:38 +0200

    removed every method, the hash-like concept isnt this great

--- lib/wormhole/config.rb
+++ lib/wormhole/config.rb
@@ -1,23 +1,8 @@
 module Wormhole
   class Config
-    # since we want a struct like concatination of indexes, every public method will be prefixed by wormhole_
-    # this looks like a dirty woraround, but I'm thinking about some kind of a dynamic alias-system vie method_missing
-    #  but until now, we will use wormhole_*
-
+    
     def initialize
       @hash = {}
-    end
-
-    def wormhole_size
-      @hash.select{|k,v| v.class != Wormhole::Config or v.wormhole_size > 0 }.size
-    end
-
-    def []=(key,value)
-      @hash[key] = value
-    end
-
-    def [](key)
-      @hash[key] ||= self.class.new
     end
 
   end
