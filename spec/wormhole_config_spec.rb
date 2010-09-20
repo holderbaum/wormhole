f84f5a16029dcdc1b43f23bc5cfa6afd212f1516
Jakob Holderbaum <privat@techfolio.de>
2010-09-20 07:22:53 +0200

    implemeted a second boundary test for empty? and a whole bunch of tests for *?

--- spec/wormhole_config_spec.rb
+++ spec/wormhole_config_spec.rb
@@ -35,11 +35,49 @@ describe Wormhole::Config do
     
   end
 
-  context "when _attr_name_? of a implicit created attr is called, it" do
-    before { @config.bar.fooze = "boofar"}
+  context "when only implicit attributes, it" do
+    before { @config.aa.bb? }
+    specify { @config.should be_empty }
+  end
+
+  context "when a real attr comes in addition, it" do
+    before do
+      @config.foo.bar?
+      @config.bar = "fooze"
+    end
+    specify { @config.should_not be_empty }
+    
+  end
+
+  context "when _attr_name_? of a implicit created attr via *= is called, it" do
+    before { @config.bar.fooze = 42 }
+    specify { @config.bar?.should be_true }
+  end
+
+  context "when _attr_name_? of a implicit created attr via *? is called, it" do
+    before { @config.bar.fooze? }
     specify { @config.bar?.should be_false }
   end
 
+  context "when _attr_name_? of a multiple times implicit created attr via *? is called, it" do
+    before do
+      @config.foo.bar?
+      @config.foo.baz?
+      @config.foo.foo? 
+    end
+    specify { @config.foo?.should be_false }
+  end
+
+  context "when attr_name_? of a implicit created attr via *? and *= is called, it" do
+    before do
+      @config.foo.bar = 42
+      @config.foo.baz = "zomg"
+      @config.foo.om?
+    end
+    specify { @config.foo?.should be_true }
+    
+  end
+  
   #context "when created" do
     #specify { @config.wormhole_size.should == 0 }
   #end
