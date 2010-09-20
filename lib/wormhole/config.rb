61bbd8f6ca80a5be53518f98ce73fee65d2af41d
Jakob Holderbaum <privat@techfolio.de>
2010-09-20 07:29:47 +0200

    implemented recursive empty? method and based on this a nice *? method_midding branch

--- lib/wormhole/config.rb
+++ lib/wormhole/config.rb
@@ -5,11 +5,26 @@ module Wormhole
       @hash = {}
     end
 
+    def empty?  
+      return @hash.select do |k,v| 
+        v.class != Wormhole::Config or (v.class == Wormhole::Config and !v.empty?) 
+      end.size == 0
+    end
+
     def method_missing(meth, *args, &blk)
       if meth.to_s =~ /[a-zA-Z0-9]*=/
         return @hash[meth.to_s[0..-2].to_sym] = args[0]
       elsif meth.to_s =~ /[a-zA-Z0-9]*\?/
-        return !@hash[meth.to_s[0..-2].to_sym].nil?
+        meth = meth.to_s[0..-2].to_sym
+        if @hash[meth].nil?
+          return false
+        else
+          if @hash[meth].class == Wormhole::Config and @hash[meth].empty?
+            return  false
+          else
+            return true
+          end
+        end
       elsif @hash[meth].nil?
         return @hash[meth] = self.class.new
       else
