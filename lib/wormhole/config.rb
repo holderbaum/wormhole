dfb4c3c67386f9e9ab2375bb8ab0fb0c3d4e8bf3
Peter Suschlik <peter@suschlik.de>
2010-09-21 21:55:29 +0200

    DRY up check for empty values.

--- lib/wormhole/config.rb
+++ lib/wormhole/config.rb
@@ -7,16 +7,21 @@ module Wormhole
 
     def empty?  
       @hash.all? do |_, value|
-        value.is_a?(Config) && value.empty?
+        _empty_value?(value)
       end
     end
+
+    def _empty_value?(value) # :nodoc:
+      value.is_a?(Config) && value.empty?
+    end
+    private :_empty_value?
 
     def method_missing(meth, *args, &blk)
       if meth.to_s =~ /[a-zA-Z0-9]*=/
         @hash[meth.to_s[0..-2].to_sym] = args[0]
       elsif meth.to_s =~ /[a-zA-Z0-9]*\?/
         meth = meth.to_s[0..-2].to_sym
-        (value = @hash[meth]) && !(value.is_a?(Config) && value.empty?)
+        (value = @hash[meth]) && !_empty_value?(value)
       elsif @hash[meth].nil?
         @hash[meth] = self.class.new
       else
