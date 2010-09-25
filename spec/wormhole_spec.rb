c9b9bfb3dc3229aef6738dc047016938e1a3da30
Jakob Holderbaum <privat@techfolio.de>
2010-09-25 21:27:49 +0200

    defined and instanciated a test_class in the before test routine for Wormhole itself

--- spec/wormhole_spec.rb
+++ spec/wormhole_spec.rb
@@ -1,8 +1,18 @@
 require 'spec_helper'
 
 describe Wormhole do
+  
+  before do
+    class TestClass
+      include Wormhole
+    end
+
+    @test_class = TestClass.new
+  end
+  
   it 'should have a VERSION' do
     Wormhole::VERSION::STRING.should == "0.0.1"
   end
+
 end
 
