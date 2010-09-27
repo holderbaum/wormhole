57a6d02debfba8ac862693b5c2365560bb6b4c26
Jakob Holderbaum <privat@techfolio.de>
2010-09-27 11:14:57 +0200

    add documentation for Wormhole itself

--- lib/wormhole/wormhole.rb
+++ lib/wormhole/wormhole.rb
@@ -3,14 +3,31 @@ module Wormhole
 
     @@namespaces = {}
 
+    # returns the class that will be used as configuration-container
+    #
+    # @return [Const] classname
     def config_backend
       @@config_backend ||= Config
     end
 
+    # sets the configuration-container class
+    #
+    # @return [Const] new classname
     def config_backend=(config_class)
       @@config_backend = config_class
     end
 
+    # creates a new wormhole namespace and yields a new config_backend instance
+    #
+    # @example Usage:
+    #   Wormhole.create(:foo) do |config|
+    #     config.bar = "fooze"
+    #     config.class # => Wormhole::Config
+    #   end
+    #
+    # @param [Symbol] namespace
+    # @yield [config_backend.new] new config_backend instance 
+    # @return [nil] TODO: define return-value
     def create(namespace)
       yield(@@namespaces[namespace] ||= config_backend.new)
     end
