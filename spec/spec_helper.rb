ca513b1833f5004de773487495ee97fb05104c34
Peter Suschlik <ps@neopoly.de>
2010-09-27 10:31:05 +0200

    No need for TestClass. Use anonymous class.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,1 @@
 require 'wormhole'
-
-def reset_test_class
-  Object.send(:remove_const,:TestClass)
-end
--- spec/wormhole_spec.rb
+++ spec/wormhole_spec.rb
@@ -3,58 +3,53 @@ require 'spec_helper'
 describe Wormhole do
   
   before do
-    class TestClass
+    @test_class = Class.new do
       include Wormhole
     end
   end
  
-  after(:each) do
-    reset_test_class
-  end
-
-
   it 'should have a VERSION' do
     Wormhole::VERSION::STRING.should == "0.0.1"
   end
 
   describe "rspec test environment" do
-    it "should set TestClass definition before every test" do
-      Module::constants.include?("TestClass").should be_true
-      def TestClass.monkey
+    it "should set test_class definition before every test" do
+      @test_class.class.should == Class
+      def @test_class.monkey
         puts "patch"
       end
     end
 
-    it "should reset the TestClass after every test" do
-      TestClass.methods.include?("monkey").should be_false
+    it "should reset the test_class after every test" do
+      @test_class.methods.include?("monkey").should be_false
     end
   end
 
   describe "self.config_backend" do
     it "should be set by default to Wormhole::Config" do
-      TestClass.config_backend.should == Wormhole::Config
+      @test_class.config_backend.should == Wormhole::Config
     end
 
     it "should be configurable" do
-      TestClass.config_backend = Hash
-      TestClass.config_backend.should == Hash
+      @test_class.config_backend = Hash
+      @test_class.config_backend.should == Hash
     end
   end
 
   describe "self.create" do
     it "should yield a block with an instance of the constance_backend as argument" do
-      TestClass.create(:foo) do |config|
+      @test_class.create(:foo) do |config|
         config.is_a?(Wormhole::Config).should be_true
       end
     end
 
     it "should yield a block with the same config instance different times under one namespace " do
       instance = nil
-      TestClass.create(:foo) do |config|
+      @test_class.create(:foo) do |config|
         instance = config
       end
 
-      TestClass.create(:foo) do |config|
+      @test_class.create(:foo) do |config|
         config.should == instance
       end
     end
