ed5992348b201ed0212bee71f55a11b6b5eda6ad
Jakob Holderbaum <privat@techfolio.de>
2010-10-01 10:38:11 +0200

    put the 'class methods for config_backend' in the singleton of self -- dont use class-variables :)

--- lib/wormhole/wormhole.rb
+++ lib/wormhole/wormhole.rb
@@ -1,24 +1,23 @@
 module Wormhole
   class Instance
 
-    @@namespaces = {}
+    @namespaces = {}
 
-    # returns the class that will be used as configuration-container
-    #
-    # @return [Const] classname
-    def self.config_backend
-      if self.class.class_variable_defined?("@@config_backend")
-        self.class.send :class_variable_get, "@@config_backend"
-      else
-        self.class.send :class_variable_set, "@@config_backend", Config
+    class << self # change self scope to metaclass of Instance
+
+      # returns the class that will be used as configuration-container
+      #
+      # @return [Const] classname
+      def config_backend
+        @config_backend ||= Config
       end
-    end
 
-    # sets the configuration-container class
-    #
-    # @return [Const] new classname
-    def self.config_backend=(config_class)
-      self.class.send :class_variable_set, "@@config_backend", config_class
+      # sets the configuration-container class
+      #
+      # @return [Const] new classname
+      def config_backend=(config_class)
+        @config_backend = config_class
+      end
     end
 
     # creates a new wormhole namespace and yields a new config_backend instance
