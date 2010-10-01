d26f6df6ab6e569f3cf3ad04126ad47cbc681397
Jakob Holderbaum <privat@techfolio.de>
2010-10-01 10:44:44 +0200

    copy the create 'class-metod' into the singleton -- we dont have any @@ magic now

--- lib/wormhole/wormhole.rb
+++ lib/wormhole/wormhole.rb
@@ -18,21 +18,22 @@ module Wormhole
       def config_backend=(config_class)
         @config_backend = config_class
       end
-    end
 
-    # creates a new wormhole namespace and yields a new config_backend instance
-    #
-    # @example Usage:
-    #   Wormhole.create(:foo) do |config|
-    #     config.bar = "fooze"
-    #     config.class # => Wormhole::Config
-    #   end
-    #
-    # @param [Symbol] namespace
-    # @yield [config_backend.new] new config_backend instance 
-    # @return [nil] TODO: define return-value
-    def self.create(namespace)
-      yield(@@namespaces[namespace] ||= config_backend.new)
+
+      # creates a new wormhole namespace and yields a new config_backend instance
+      #
+      # @example Usage:
+      #   Wormhole.create(:foo) do |config|
+      #     config.bar = "fooze"
+      #     config.class # => Wormhole::Config
+      #   end
+      #
+      # @param [Symbol] namespace
+      # @yield [config_backend.new] new config_backend instance 
+      # @return [nil] TODO: define return-value
+      def create(namespace)
+        yield(@namespaces[namespace] ||= config_backend.new)
+      end
     end
   end
 
