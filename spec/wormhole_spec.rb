ca9b77b72d4469b44ed399c05d4281ab56969cb5
Jakob Holderbaum <privat@techfolio.de>
2010-10-09 00:20:25 +0200

    complete the testbench for to_javascript calls without arguments

--- spec/wormhole_spec.rb
+++ spec/wormhole_spec.rb
@@ -200,6 +200,32 @@ describe Wormhole do
       cut_json( @wormhole.to_javascript ).should == { "foo" => { "bar" => "baz" } }
     end
 
+    it "should create a javascript object from all namespaces" do
+      @wormhole.create(:foo) do |config|
+        config.fooze = 42
+      end
+
+      @wormhole.create(:bar) do |config|
+        config.baz = 42
+      end
+
+      cut_json( @wormhole.to_javascript ).should == { "foo" => { "fooze" => 42 }, "bar" => { "baz" => 42 } }
+    end
+
+    it "should create a javascript object under consideration of the merge calls" do
+      @wormhole.create(:foo) do |config|
+        config.bar = 42
+      end
+
+      Thread.new do
+        @wormhole.merge(:foo) do |config|
+          config.baz = 43
+        end
+
+        cut_json( @wormhole.to_javascript ).should == { "foo" => { "bar" => 42, "baz" => 43 } }
+      end.join
+    end
+
   end
 
 end
