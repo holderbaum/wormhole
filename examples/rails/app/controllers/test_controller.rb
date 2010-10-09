901d9830b81bad64b5d1c918b789e259c9c68f40
Jakob Holderbaum <privat@techfolio.de>
2010-10-09 22:04:37 +0200

    just a typo fix, sure is action and view the same thread ^^

--- examples/rails/app/controllers/test_controller.rb
+++ examples/rails/app/controllers/test_controller.rb
@@ -1,12 +1,11 @@
 class TestController < ApplicationController
 
   def index
-    if params[:numer]
+    if params[:number]
       wormhole.merge(:config) do |c|
         c.test = params[:number]
       end
     end
-    # it seems, the view execution is NOT the same thread like the action itself ???
   end
 
 end
