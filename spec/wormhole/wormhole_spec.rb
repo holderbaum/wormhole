b4dc49ac8e8da00cd8be3c874d5d7d9ae22b504b
Jakob Holderbaum <privat@techfolio.de>
2010-10-12 20:26:35 +0200

    move the spec into spec/wormhole

--- spec/wormhole/wormhole_spec.rb
+++ spec/wormhole/wormhole_spec.rb
@@ -6,6 +6,10 @@ describe Wormhole do
     @wormhole = Wormhole::Instance.dup
   end
 
+  it 'should have a VERSION' do
+    Wormhole::VERSION::STRING.should == "0.1.0"
+  end
+
   describe "rspec test environment" do
 
     it "set the config_backend value" do
@@ -175,6 +179,90 @@ describe Wormhole do
         end
       end.join # thread
       
+    end
+  end
+
+  describe "to_javascript without arguments" do
+
+    it "should create an empty javascript object if no namespaces where created" do
+      cut_json(@wormhole.to_javascript).should == {}
+    end
+
+    it "should create a javascript object with the namespace as the first key" do
+      @wormhole.create(:foo) do |config|
+        config.bar = "baz"
+      end
+
+      cut_json( @wormhole.to_javascript ).should == { "foo" => { "bar" => "baz" } }
+    end
+
+    it "should create a javascript object from all namespaces" do
+      @wormhole.create(:foo) do |config|
+        config.fooze = 42
+      end
+
+      @wormhole.create(:bar) do |config|
+        config.baz = 42
+      end
+
+      cut_json( @wormhole.to_javascript ).should == { "foo" => { "fooze" => 42 }, "bar" => { "baz" => 42 } }
+    end
+
+    it "should create a javascript object under consideration of the merge calls" do
+      @wormhole.create(:foo) do |config|
+        config.bar = 42
+      end
+
+      Thread.new do
+        @wormhole.merge(:foo) do |config|
+          config.baz = 43
+        end
+
+        cut_json( @wormhole.to_javascript ).should == { "foo" => { "bar" => 42, "baz" => 43 } }
+      end.join
+    end
+
+  end
+
+  describe "to_javascript with arguments" do
+    
+    it "should create a javascript object containing only the via args given namespace" do
+      @wormhole.create(:foo) do |config|
+        config.bar = 42
+      end
+
+      @wormhole.create(:bar) do |config|
+        config.baz = 43
+      end
+
+      cut_json( @wormhole.to_javascript(:foo) ).should == { "foo" => { "bar" => 42 } }
+    end
+
+    it "should create a javascript object containing only the via args given namespaces under consideration of the merge calls" do
+      @wormhole.create(:foo) do |config|
+        config.bar = 42
+      end
+
+      Thread.new do
+        @wormhole.merge(:foo) do |config|
+          config.baz = 43
+        end
+
+        cut_json( @wormhole.to_javascript(:foo) ).should == { "foo" => { "bar" => 42, "baz" => 43 } }
+      end.join
+      
+    end
+
+    it "should create a javascript object containing two given namespaces" do
+      @wormhole.create(:foo) do |config|
+        config.fooze = 42
+      end
+
+      @wormhole.create(:bar) do |config|
+        config.baz = 42
+      end
+
+      cut_json( @wormhole.to_javascript(:bar, :foo) ).should == { "foo" => { "fooze" => 42 }, "bar" => { "baz" => 42 } }
     end
   end
 
