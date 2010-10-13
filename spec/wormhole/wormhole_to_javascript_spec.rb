9ee0afe6073b4b6dc5df68a01c3ba3979180f288
Jakob Holderbaum <privat@techfolio.de>
2010-10-13 15:29:25 +0200

    add a test for general correct creation of a js-declaration and one for creating an empty hash from an empty wormhole.

--- spec/wormhole/wormhole_to_javascript_spec.rb
+++ spec/wormhole/wormhole_to_javascript_spec.rb
@@ -8,6 +8,18 @@ describe Wormhole do
 
   describe "to_javascript without arguments" do
 
+    it "should render a correct javascript declaration" do
+      @wormhole.create(:foo) do |c|
+        c.bar = 42
+      end
+
+      correct_declaration?(@wormhole.to_javascript).should be_true
+    end
+
+    it "should render an empty object from an empty wormhole" do
+      cut_json( @wormhole.to_javascript ).should == {}
+    end
+
   end
 
   describe "to_javascript with arguments" do
