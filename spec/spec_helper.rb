d7c53063fbc8fc49bdbba385a621f40f251df23a
Jakob Holderbaum <privat@techfolio.de>
2010-10-13 15:28:38 +0200

    add a spec_helper for determination of correct javscript object declaration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,9 @@
 require 'wormhole'
 
+def correct_declaration?(javascript, object = "Wormhole")
+  !/^var\s*#{object}\s*=\s*.*;$/.match(javascript).nil?
+end
+
 def cut_json(javascript, object = "Wormhole")
   JSON.parse /^var\s*#{object}\s*=\s*(\{.*\});$/.match(javascript)[1]
 end
