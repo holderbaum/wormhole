65956dc83ec0eea812aab70b9cfbccbe64b0f675
Jakob Holderbaum <privat@techfolio.de>
2010-10-14 09:52:52 +0200

    add respond_to Wormhole and an example API-Userstory that describes how this entire concept SHOULD be used.

--- spec/wormhole/api_spec.rb
+++ spec/wormhole/api_spec.rb
@@ -6,14 +6,34 @@ describe "Environment" do
     should respond_to(:wormhole)
   end
 
+  it "should respond to Wormhole" do
+    should respond_to(:Wormhole)
+  end
+
   describe "wormhole" do
 
-    # through the testing against Instance, we're able to assume,
-    # that the entire Instance functionality is now accessible
-    # through wormhole.
-    # This makes any further API-testing futile
     it "should return an instance of Wormhole::Instance" do
       wormhole.should be_a(Wormhole::Instance)
+    end
+
+  end
+
+  describe "API" do
+  
+    it "should enable a perfect story" do
+      Wormhole.create(:foo) do |c|
+        c.bar = 42
+      end
+
+      threaded do
+        Wormhole(:foo) do |c|
+          c.bar = 23
+        end
+
+        cut_json( Wormhole.to_javascript ).should == { "foo" => {"bar" => 23} }
+      end
+
+      cut_json( Wormhole.to_javascript ).should == { "foo" => {"bar" => 42} }
     end
 
   end
